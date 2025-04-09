ad67866548ee7b40ee1eaa0d2cd189c8ee676262
shayklein165 <shaylior@post.bgu.ac.il>
2025-04-09 12:53:35 +0300

    requirment 1 Board Tests commit

--- Tests/BoardTests.cs
+++ Tests/BoardTests.cs
@@ -8,5 +8,107 @@ namespace Tests
 {
     class BoardTests
     {
+        public bool TestDeleteBoardNegativeCase()
+        {
+            BoardService b = new BoardService();
+            Response res = b.deleteBoard("name");
+            if(res.ErrorMsg == null)
+            {
+                return false;
+            }
+            return true;
+
+        }
+
+        public bool TestDeleteBoardPositiveCase()
+        {
+            BoardService b = new BoardService();
+            b.createBoard("name");
+            Response res = b.deleteBoard("name");
+            if (res.ErrorMsg != null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+        public bool TestCreateBoardNegativeCase()
+        {
+            BoardService b = new BoardService();
+            b.createBoard("name");
+            Response res = b.createBoard("name");
+            if (res.ErrorMsg == null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool TestCreateBoardPositiveCase()
+        {
+            BoardService b = new BoardService();
+            b.createBoard("name");
+            Response res = b.getBoard();
+            if (res.ErrorMsg == null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool TestGetBoardNegativeCase()
+        {
+            BoardService b = new BoardService();
+            b.createBoard("name");
+            Response res = b.getBoard();
+
+            if (res.ErrorMsg == null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool TestGetBoardPositiveCase()
+        {
+            BoardService b = new BoardService();
+            b.createBoard("name");
+            Response res = b.getBoard();
+
+            if (res.ErrorMsg != null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool TestLimitTasksNegativeCase()
+        {
+            BoardService b = new BoardService();
+            b.limitTasks("name", 0);
+
+            Response res = b.createBoard("name");
+
+            if (res.ErrorMsg == null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool TestLimitTasksPositiveCase()
+        {
+            BoardService b = new BoardService();
+            b.limitTasks("name", 0);
+
+            Response res = b.createBoard("name");
+
+            if (res.ErrorMsg != null)
+            {
+                return false;
+            }
+            return true;
+        }
     }
 }
