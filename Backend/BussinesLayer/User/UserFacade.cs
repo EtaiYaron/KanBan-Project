c27106fd75b0483ee5184601a8b15f977c6bd2f5
Max Liberman <maxli@post.bgu.ac.il>
2025-04-19 20:14:17 +0300

    finished register method

--- Backend/BussinesLayer/User/UserFacade.cs
+++ Backend/BussinesLayer/User/UserFacade.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using IntroSE.Kanban.Backend.BussinesLayer.Cross_Cutting;
 
@@ -25,6 +26,8 @@ namespace IntroSE.Kanban.Backend.BussinesLayer.User
 
             if (!users.ContainsKey(email))
                 throw new Exception("User " + email + "doesn't exist");
+            if (authFacade.isLoggedIn(email))
+                throw new Exception("User already logged in");
 
             UserBL user = users[email];
             if (user.Login(password))
@@ -37,11 +40,19 @@ namespace IntroSE.Kanban.Backend.BussinesLayer.User
 
         public UserBL Register(string email, string password)
         {
-            if (password == null) throw new ArgumentNullException("password");
             if (email == null) throw new ArgumentNullException("email");
+            if (password == null) throw new ArgumentNullException("password");
 
+            if (!IsValidEmail(email)) throw new Exception("Invalid email");
             if (!isValidPassword(password)) throw new Exception("Invalid password");
-            return null;
+            if (users.ContainsKey(email))
+                throw new Exception("User " + email + "already exist");
+            if (authFacade.isLoggedIn(email))
+                throw new Exception("User already logged in");
+
+            UserBL user = new UserBL(email, password);
+            authFacade.Login(email);
+            return user;
         }
 
         public UserBL Logout(string email)
@@ -53,6 +64,12 @@ namespace IntroSE.Kanban.Backend.BussinesLayer.User
         {
             if (password.Length < 6 || password.Length > 20) return false;
             return password.Any(char.IsUpper) && password.Any(char.IsLower) && password.Any(char.IsDigit);
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            string emailPattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            return Regex.IsMatch(email, emailPattern);
         }
     }
 }
