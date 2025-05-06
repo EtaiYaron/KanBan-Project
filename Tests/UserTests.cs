4801a50efc711ed86e52f7f83edea50c30e28d21
Etai <yaronet@post.bgu.ac.il>
2025-05-06 17:48:08 +0300

    adding tests and checking diffrent cases.

--- Tests/UserTests.cs
+++ Tests/UserTests.cs
@@ -34,14 +34,32 @@ namespace Tests
             {
                 Console.WriteLine("TestUserRegisterPositiveCase: Failed");
             }
-            tests = TestUserLoginNegativeCase();
+            tests = TestUserRegisterPositiveCase1();
             if (tests)
             {
-                Console.WriteLine("TestUserLoginNegativeCase: Passed");
+                Console.WriteLine("TestUserRegisterPositiveCase1: Passed");
             }
             else
             {
-                Console.WriteLine("TestUserLoginNegativeCase: Failed");
+                Console.WriteLine("TestUserRegisterPositiveCase1: Failed");
+            }
+            tests = TestUserRegisterNegativeCase();
+            if (tests)
+            {
+                Console.WriteLine("TestUserRegisterNegativeCase: Passed");
+            }
+            else
+            {
+                Console.WriteLine("TestUserRegisterNegativeCase: Failed");
+            }
+            tests = TestUserRegisterNegativeCase1();
+            if (tests)
+            {
+                Console.WriteLine("TestUserRegisterNegativeCase1: Passed");
+            }
+            else
+            {
+                Console.WriteLine("TestUserRegisterNegativeCase1: Failed");
             }
             tests = TestUserLoginPositiveCase();
             if (tests)
@@ -52,6 +70,15 @@ namespace Tests
             {
                 Console.WriteLine("TestUserLoginPositiveCase: Failed");
             }
+            tests = TestUserLoginPositiveCase1();
+            if (tests)
+            {
+                Console.WriteLine("TestUserLoginPositiveCase1: Passed");
+            }
+            else
+            {
+                Console.WriteLine("TestUserLoginPositiveCase1: Failed");
+            }
             tests = TestUserLoginNegativeCase();
             if (tests)
             {
@@ -60,6 +87,15 @@ namespace Tests
             else
             {
                 Console.WriteLine("TestUserLoginNegativeCase: Failed");
+            }
+            tests = TestUserLoginNegativeCase1();
+            if (tests)
+            {
+                Console.WriteLine("TestUserLoginNegativeCase1: Passed");
+            }
+            else
+            {
+                Console.WriteLine("TestUserLoginNegativeCase1: Failed");
             }
             tests = TestUserLogoutPositiveCase();
             if (tests)
@@ -70,6 +106,15 @@ namespace Tests
             {
                 Console.WriteLine("TestUserLogoutPositiveCase: Failed");
             }
+            tests = TestUserLogoutPositiveCase1();
+            if (tests)
+            {
+                Console.WriteLine("TestUserLogoutPositiveCase1: Passed");
+            }
+            else
+            {
+                Console.WriteLine("TestUserLogoutPositiveCase1: Failed");
+            }
             tests = TestUserLogoutNegativeCase();
             if (tests)
             {
@@ -78,6 +123,15 @@ namespace Tests
             else
             {
                 Console.WriteLine("TestUserLogoutNegativeCase: Failed");
+            }
+            tests = TestUserLogoutNegativeCase1();
+            if (tests)
+            {
+                Console.WriteLine("TestUserLogoutNegativeCase1: Passed");
+            }
+            else
+            {
+                Console.WriteLine("TestUserLogoutNegativeCase1: Failed");
             }
         }
 
@@ -90,15 +144,34 @@ namespace Tests
             }
             return true;
         }
+        public bool TestUserRegisterPositiveCase1()
+        {
+            Response res = JsonSerializer.Deserialize<Response>(us.Register("Amztia@post.co.il", "Amztia1"));
+            if (res.ErrorMessage != null)
+            {
+                return false;
+            }
+            return true;
+        }
         public bool TestUserRegisterNegativeCase()
         {
-            Response res = JsonSerializer.Deserialize < Response > (us.Register("Amztia@gmail.com",  "amztia1"));
+            Response res = JsonSerializer.Deserialize < Response > (us.Register("Amztia@post.co.il",  "amztia1"));
             if (res.ErrorMessage != null)
             {
                 return true;
             }
             return false;
         }
+        public bool TestUserRegisterNegativeCase1()
+        {
+            Response res = JsonSerializer.Deserialize<Response>(us.Register("Amztiapost.co.il", "Amztia1"));
+            if (res.ErrorMessage != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
 
         public bool TestUserLoginPositiveCase()
         {
@@ -110,9 +183,29 @@ namespace Tests
             }
             return true;
         }
+        public bool TestUserLoginPositiveCase1()
+        {
+            us.Logout("Amztia@post.co.il");
+            Response res = JsonSerializer.Deserialize<Response>(us.Login("Amztia@post.co.il", "Amztia1"));
+            if (res.ErrorMessage != null)
+            {
+                return false;
+            }
+            return true;
+        }
+
         public bool TestUserLoginNegativeCase()
         {
             Response res = JsonSerializer.Deserialize < Response > (us.Login("etaiyaron@gmail.com", "password1"));
+            if (res.ErrorMessage != null)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool TestUserLoginNegativeCase1()
+        {
+            Response res = JsonSerializer.Deserialize<Response>(us.Login("@gmail.com", "Password1"));
             if (res.ErrorMessage != null)
             {
                 return true;
@@ -128,10 +221,27 @@ namespace Tests
             }
             return true;
         }
+        public bool TestUserLogoutPositiveCase1()
+        {
+            Response res = JsonSerializer.Deserialize<Response>(us.Logout("Amztia@post.co.il"));
+            if (res.ErrorMessage != null)
+            {
+                return false;
+            }
+            return true;
+        }
         public bool TestUserLogoutNegativeCase()
         {
-            us.Login("etaiyaron@gmail.com", "Password1");
             Response res = JsonSerializer.Deserialize < Response > (us.Logout("EtaiYaron"));
+            if (res.ErrorMessage != null)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool TestUserLogoutNegativeCase1()
+        {
+            Response res = JsonSerializer.Deserialize<Response>(us.Logout("Amztia@post.co.il"));
             if (res.ErrorMessage != null)
             {
                 return true;
