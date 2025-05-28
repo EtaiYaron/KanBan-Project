69ba1148716654381a6c466e8e645e08fe05028b
shayklein165 <shaylior@post.bgu.ac.il>
2025-05-28 11:13:25 +0300

    deleted assignTaskToUSer

--- Backend/ServiceLayer/BoardService.cs
+++ Backend/ServiceLayer/BoardService.cs
@@ -308,20 +308,5 @@ namespace IntroSE.Kanban.Backend.ServiceLayer
                 return JsonSerializer.Serialize(response);
             }
         }
-
-        public string AssignTaskToUser(string email, string boardName, int taskId, string emailTo)
-        {
-            try
-            {
-                boardFacade.AssignTaskToUser(email, boardName, taskId, emailTo);
-                Response response = new Response();
-                return JsonSerializer.Serialize(response);
-            }
-            catch (Exception ex)
-            {
-                Response response = new Response(ex.Message);
-                return JsonSerializer.Serialize(response);
-            }
-        }
     }
 }
