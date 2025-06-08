e7857b4c0c3ce5f2a368ddb356ac19c88d38e747
shayklein165 <shaylior@post.bgu.ac.il>
2025-06-08 15:31:58 +0300

    added CreateBoardScreen xaml

---
+++ Frontend/ViewModel/CreateBoardVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroSE.Kanban.Frontend.ViewModel
+{
+    internal class CreateBoardVM: Notifiable
+    {
+        private string name;
+        private string owner;
+        private string errorMessage;
+
+        BoardController boardController = ControllerFactory.Instance.BoardController;
+
+        public CreateBoardVM()
+        {
+            name = string.Empty;
+            owner = string.Empty;
+            errorMessage = string.Empty;
+        }
+
+
+    }
+}
