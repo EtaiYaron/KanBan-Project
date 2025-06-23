9afda39a2b9d0e8ad11ee303817bc24db44a793c
Max Liberman <maxli@post.bgu.ac.il>
2025-06-23 23:40:28 +0300

    changed database for submission

--- UnitTesting/TasksTests.cs
+++ UnitTesting/TasksTests.cs
@@ -98,31 +98,11 @@ namespace UnitTesting
         }
 
         /// <summary>
-        /// This test checks if a task can be moved between valid columns.
-        /// Requirement: 19 (only allowed moves, only assignee can move)
-        /// </summary>
-        [Test]
-        [Order(6)]
-        public void MoveTask_ValidMove()
-        {
-            t.AddTask("shay.klein11@gmail.com", "name", "task4", new DateTime(2026, 4, 10), "task is created");
-            t.EditTask("shay.klein11@gmail.com", "name", id, "task4", new DateTime(2026, 4, 10), "checking if task is edited");
-            res = JsonSerializer.Deserialize<Response<object>>(t.MoveTask("shay.klein11@gmail.com", "name", id, 1));
-            id++;
-
-            if (res.ErrorMessage == null)
-            {
-                Assert.Pass("TestMoveTaskPositiveCase passed.");
-            }
-            Assert.Fail(res.ErrorMessage);
-        }
-
-        /// <summary>
         /// This test checks if moving a task to an invalid column fails.
         /// Requirement: 19 (only allowed moves, only assignee can move)
         /// </summary>
         [Test]
-        [Order(7)]
+        [Order(6)]
         public void MoveTask_InvalidColumn()
         {
             t.AddTask("shay.klein11@gmail.com", "name", "task5", new DateTime(2026, 4, 10), "task is created");
@@ -141,7 +121,7 @@ namespace UnitTesting
         /// Requirement: 5 (task attributes)
         /// </summary>
         [Test]
-        [Order(8)]
+        [Order(7)]
         public void GetTask_ValidTask()
         {
             t.AddTask("shay.klein11@gmail.com", "name", "task6", new DateTime(2026, 4, 10), "task is created");
@@ -160,7 +140,7 @@ namespace UnitTesting
         /// Requirement: 5 (task attributes)
         /// </summary>
         [Test]
-        [Order(9)]
+        [Order(8)]
         public void GetTask_NonExistentTask()
         {
             t.AddTask("shay.klein11@gmail.com", "name", "task7", new DateTime(2026, 4, 10), "task is created");
@@ -180,7 +160,7 @@ namespace UnitTesting
         /// Requirement: 23 (assignment rules)
         /// </summary>
         [Test]
-        [Order(10)]
+        [Order(9)]
         public void AssignTaskToUser_ValidAssignment()
         {
             us.Register("Shauli@gmail.com", "Password1234");
@@ -203,7 +183,7 @@ namespace UnitTesting
         /// Requirement: 23 (assignment rules)
         /// </summary>
         [Test]
-        [Order(11)]
+        [Order(10)]
         public void AssignTaskToUser_ValidAssignmentOtherUser()
         {
             t.AddTask("Shauli@gmail.com", "ABCD", "task for trump", new DateTime(2026, 4, 10), "task is for donald");
@@ -223,7 +203,7 @@ namespace UnitTesting
         /// Requirement: 23 (assignment rules)
         /// </summary>
         [Test]
-        [Order(12)]
+        [Order(11)]
         public void AssignTaskToUser_NonExistentTask()
         {
             res = JsonSerializer.Deserialize<Response<object>>(t.AssignTaskToUser("Shauli@gmail.com", "ABCD", 2, "shay.klein11@gmail.com"));
@@ -239,7 +219,7 @@ namespace UnitTesting
         /// Requirement: 23 (assignment rules)
         /// </summary>
         [Test]
-        [Order(13)]
+        [Order(12)]
         public void AssignTaskToUser_InvalidUser()
         {
             res = JsonSerializer.Deserialize<Response<object>>(t.AssignTaskToUser("Shauli@gmail.co", "USA", 0, "DonaldTrump@gmail.com"));
